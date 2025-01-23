cb1895c09b09f5b8e286e7cabf740480793d03c2
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-01-23 15:42:50 -0800

    Initial Commit

---
+++ Activity one - cis 25/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Activity one - cis 25
+//
+//  Created by Subigya Parajuli on 1/23/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
